8b1cbeab7d8b4b64fe90f9e11b447ec12fba7387
Bohdan Horb <wannaalwaysbe@gmail.com>
2022-08-05 09:55:54 +0300

    Add js button to hide sidebar.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-$('body').on("DOMNodeInserted", "select", function () {
-  const a = $('.sl-prose');
-  console.log(a);
-});
+$('#testButton').click(function () {
+  const selector = '.sl-flex.sl-overflow-y-auto.sl-flex-col.sl-sticky.sl-inset-y-0.sl-pt-8.sl-bg-canvas-100.sl-border-r'
+  if ($(selector).is(':visible')) {
+    $(selector).css('display', 'none');
+  }
+  else {
+    $(selector).css('display', 'unset');
+  }
+})
