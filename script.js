d2fceb69174a173c0e9f710a9920faa231bb4226
Bohdan Horb <wannaalwaysbe@gmail.com>
2022-08-05 11:16:24 +0300

    Add js to move menu out of the stoplight tag after page rendered.

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+function highlighterAction() {
+  const selector = '.sl-flex.sl-overflow-y-auto.sl-flex-col.sl-sticky.sl-inset-y-0.sl-pt-8.sl-bg-canvas-100.sl-border-r'
+  console.log($(selector));
+  $($(selector).detach()).appendTo("#attachMenu");
+}
+
+function highlighter() {
+  /*
+    The short pause allows any required callback functions
+    to execute before actually highlighting, and allows
+    the JQuery $(document).ready wrapper to finish.
+   */
+  setTimeout(function() {
+    highlighterAction();
+  }, 200);
+}
+
+/*
+  Only trigger the highlighter after document fully loaded.  This is
+  necessary for cases where page load takes a significant length
+  of time to fully load.
+*/
+if (document.readyState == 'complete') {
+  highlighter();
+} else {
+  document.onreadystatechange = function () {
+    if (document.readyState === "complete") {
+      highlighter();
+    }
+  }
+}
+
 $('#testButton').click(function () {
   const selector = '.sl-flex.sl-overflow-y-auto.sl-flex-col.sl-sticky.sl-inset-y-0.sl-pt-8.sl-bg-canvas-100.sl-border-r'
   if ($(selector).is(':visible')) {
